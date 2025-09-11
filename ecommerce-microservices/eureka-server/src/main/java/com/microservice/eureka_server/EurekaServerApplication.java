5b948a9af20b762276d764d271ce55aa2be2d30d
lakshaybxt <bishtlakshay14@gmail.com>
2025-09-11 09:16:06 +0530

    Changed groupId

--- ecommerce-microservices/eureka-server/src/main/java/com/microservice/eureka_server/EurekaServerApplication.java
+++ ecommerce-microservices/eureka-server/src/main/java/com/microservice/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.cognivanta.eureka_server;
+package com.microservice.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ecommerce-microservices/eureka-server/src/main/java/com/microservice/eureka_server/configuration/SecurityConfig.java
+++ ecommerce-microservices/eureka-server/src/main/java/com/microservice/eureka_server/configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.cognivanta.eureka_server.configuration;
+package com.microservice.eureka_server.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
