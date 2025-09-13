d1a183febcd9fdc9628359d665e226dfe0ad7980
lakshaybxt <bishtlakshay14@gmail.com>
2025-09-13 12:12:08 +0530

    Initialized Cart Service

--- ecommerce-microservices/order-service/src/main/java/com/microservice/order_service/OrderServiceApplication.java
+++ ecommerce-microservices/order-service/src/main/java/com/microservice/order_service/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.cognivanta.order_service;
+package com.microservice.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
