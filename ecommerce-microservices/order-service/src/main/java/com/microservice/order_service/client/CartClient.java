467f7af2ffe98077101c56b5fa2cc94e65593572
lakshaybxt <bishtlakshay14@gmail.com>
2025-09-22 23:08:51 +0530

    Implemented check to reserve product

--- ecommerce-microservices/order-service/src/main/java/com/microservice/order_service/client/CartClient.java
+++ ecommerce-microservices/order-service/src/main/java/com/microservice/order_service/client/CartClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "cart-service", url = "http://localhost:8080/api/v1")
+@FeignClient(name = "cart-service", url = "http://localhost:8082/api/v1")
 public interface CartClient {
 
     @GetMapping("/cart/public")
